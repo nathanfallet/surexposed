a41f25d76ef61a8befcb37babe2789a611bf2b4b
NathanFallet <contact@nathanfallet.me>
2024-03-04 19:46:00 +0100

    Updating dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "me.nathanfallet.surexposed"
-version = "1.0.1"
+version = "1.0.2"
 
 repositories {
     mavenCentral()
@@ -26,19 +26,19 @@ kotlin {
 
     applyDefaultHierarchyTemplate()
 
-    val exposedVersion = "0.47.0"
+    val exposedVersion = "0.48.0"
 
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
                 api("org.jetbrains.exposed:exposed-core:$exposedVersion")
                 api("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
                 api("org.jetbrains.exposed:exposed-dao:$exposedVersion")
 
-                api("me.nathanfallet.usecases:usecases:1.5.6")
+                api("me.nathanfallet.usecases:usecases:1.6.0")
             }
         }
         val jvmTest by getting {
